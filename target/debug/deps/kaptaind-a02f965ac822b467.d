21942bb10c894d7c07880b70da707dcf0411c43a
elci-group <rory.spring@icloud.com>
2026-04-02 22:45:28 +0100

    kaptaind: Patch -> v0.1.9 [api-stable; paths=4; api_touches=0; deps=0; runtime=0; score=0.104; cluster=a0783a7d-f748-4a73-bbd3-436d44dcfe8d]

---
+++ target/debug/deps/kaptaind-a02f965ac822b467.d
@@ -0,0 +1,30 @@
+/home/adminx/kaptaind/target/debug/deps/kaptaind-a02f965ac822b467.d: src/lib.rs src/cluster/mod.rs src/cluster/engine.rs src/commit/mod.rs src/commit/orchestrator.rs src/config/mod.rs src/config/loader.rs src/daemon/mod.rs src/daemon/runtime.rs src/daemon/scheduler.rs src/diff/mod.rs src/diff/api.rs src/diff/ast.rs src/diff/text.rs src/git/mod.rs src/git/repo.rs src/push/mod.rs src/push/controller.rs src/version/mod.rs src/version/semver.rs src/watcher/mod.rs src/watcher/fs.rs src/weight/mod.rs src/weight/calculator.rs
+
+/home/adminx/kaptaind/target/debug/deps/libkaptaind-a02f965ac822b467.rlib: src/lib.rs src/cluster/mod.rs src/cluster/engine.rs src/commit/mod.rs src/commit/orchestrator.rs src/config/mod.rs src/config/loader.rs src/daemon/mod.rs src/daemon/runtime.rs src/daemon/scheduler.rs src/diff/mod.rs src/diff/api.rs src/diff/ast.rs src/diff/text.rs src/git/mod.rs src/git/repo.rs src/push/mod.rs src/push/controller.rs src/version/mod.rs src/version/semver.rs src/watcher/mod.rs src/watcher/fs.rs src/weight/mod.rs src/weight/calculator.rs
+
+/home/adminx/kaptaind/target/debug/deps/libkaptaind-a02f965ac822b467.rmeta: src/lib.rs src/cluster/mod.rs src/cluster/engine.rs src/commit/mod.rs src/commit/orchestrator.rs src/config/mod.rs src/config/loader.rs src/daemon/mod.rs src/daemon/runtime.rs src/daemon/scheduler.rs src/diff/mod.rs src/diff/api.rs src/diff/ast.rs src/diff/text.rs src/git/mod.rs src/git/repo.rs src/push/mod.rs src/push/controller.rs src/version/mod.rs src/version/semver.rs src/watcher/mod.rs src/watcher/fs.rs src/weight/mod.rs src/weight/calculator.rs
+
+src/lib.rs:
+src/cluster/mod.rs:
+src/cluster/engine.rs:
+src/commit/mod.rs:
+src/commit/orchestrator.rs:
+src/config/mod.rs:
+src/config/loader.rs:
+src/daemon/mod.rs:
+src/daemon/runtime.rs:
+src/daemon/scheduler.rs:
+src/diff/mod.rs:
+src/diff/api.rs:
+src/diff/ast.rs:
+src/diff/text.rs:
+src/git/mod.rs:
+src/git/repo.rs:
+src/push/mod.rs:
+src/push/controller.rs:
+src/version/mod.rs:
+src/version/semver.rs:
+src/watcher/mod.rs:
+src/watcher/fs.rs:
+src/weight/mod.rs:
+src/weight/calculator.rs:
---
+++ target/debug/deps/kaptaind-deb4a506981a50b6.d
@@ -0,0 +1,5 @@
+/home/adminx/kaptaind/target/debug/deps/kaptaind-deb4a506981a50b6.d: src/main.rs
+
+/home/adminx/kaptaind/target/debug/deps/kaptaind-deb4a506981a50b6: src/main.rs
+
+src/main.rs:
